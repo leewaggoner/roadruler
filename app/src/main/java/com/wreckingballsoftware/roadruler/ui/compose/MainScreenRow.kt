91528feaa24242bc2a30fd72efa35ebdffed73b1
Lee Waggoner <lwaggoner@roadrunner.com>
2024-03-11 12:36:09 -0700

    Created a main screen row element

--- app/src/main/java/com/wreckingballsoftware/roadruler/ui/compose/MainScreenRow.kt
+++ app/src/main/java/com/wreckingballsoftware/roadruler/ui/compose/MainScreenRow.kt
@@ -1,8 +1,57 @@
 package com.wreckingballsoftware.roadruler.ui.compose
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.wreckingballsoftware.roadruler.ui.theme.customTypography
+import com.wreckingballsoftware.roadruler.ui.theme.dimensions
 
 @Composable
-fun MainScreenRow() {
-    //TODO
+fun MainScreenRow(
+    startText: String,
+    centerText: String,
+    endText: String,
+    modifier: Modifier = Modifier,
+    maxLines: Int = 1,
+    onClick: () -> Unit
+) {
+    Row(
+        modifier = modifier.then(
+            Modifier
+                .clickable(onClick = onClick)
+        ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimensions.rowSpaceSmall),
+    ) {
+        EllipsizedText(
+            modifier = Modifier
+                .padding(vertical = MaterialTheme.dimensions.spaceMedium)
+                .weight(1f),
+            text = startText,
+            style = MaterialTheme.customTypography.startBody,
+            maxLines = maxLines,
+        )
+        EllipsizedText(
+            modifier = Modifier
+                .padding(vertical = MaterialTheme.dimensions.spaceMedium)
+                .weight(1f),
+            //TODO: use actual drive name
+            text = centerText,
+            style = MaterialTheme.customTypography.centerBody,
+            maxLines = maxLines,
+        )
+        EllipsizedText(
+            modifier = Modifier
+                .padding(vertical = MaterialTheme.dimensions.spaceMedium)
+                .weight(1f),
+            text = endText,
+            style = MaterialTheme.customTypography.endBody,
+            maxLines = maxLines,
+        )
+    }
 }
--- app/src/main/java/com/wreckingballsoftware/roadruler/ui/mainscreen/MainScreen.kt
+++ app/src/main/java/com/wreckingballsoftware/roadruler/ui/mainscreen/MainScreen.kt
@@ -1,17 +1,12 @@
 package com.wreckingballsoftware.roadruler.ui.mainscreen
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Divider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -19,12 +14,11 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.wreckingballsoftware.roadruler.R
 import com.wreckingballsoftware.roadruler.domain.models.UIDrive
-import com.wreckingballsoftware.roadruler.ui.compose.EllipsizedText
+import com.wreckingballsoftware.roadruler.ui.compose.MainScreenRow
 import com.wreckingballsoftware.roadruler.ui.mainscreen.models.MainScreenEvent
 import com.wreckingballsoftware.roadruler.ui.mainscreen.models.MainScreenNavigation
 import com.wreckingballsoftware.roadruler.ui.mainscreen.models.MainScreenState
 import com.wreckingballsoftware.roadruler.ui.navigation.NavGraph
-import com.wreckingballsoftware.roadruler.ui.theme.customTypography
 import com.wreckingballsoftware.roadruler.ui.theme.dimensions
 
 @Composable
@@ -63,35 +57,15 @@ fun MainScreenContent(
             .padding(bottom = MaterialTheme.dimensions.spaceMedium)
             .padding(horizontal = MaterialTheme.dimensions.padding),
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(MaterialTheme.dimensions.rowSpaceSmall),
-        ) {
-            EllipsizedText(
-                modifier = Modifier
-                    .padding(vertical = MaterialTheme.dimensions.spaceMedium)
-                    .weight(1f),
-                text = state.transition,
-                style = MaterialTheme.customTypography.startBody,
-            )
-            EllipsizedText(
-                modifier = Modifier
-                    .padding(vertical = MaterialTheme.dimensions.spaceMedium)
-                    .weight(1f),
-                //TODO: use actual drive name
-                text = if (state.driveId > 0) stringResource(id = R.string.drive_name, state.driveId) else "",
-                style = MaterialTheme.customTypography.centerBody,
-            )
-            EllipsizedText(
-                modifier = Modifier
-                    .padding(vertical = MaterialTheme.dimensions.spaceMedium)
-                    .weight(1f),
-                text = state.currentDistance,
-                style = MaterialTheme.customTypography.endBody,
-            )
-        }
+        MainScreenRow(
+            startText = state.transition,
+            centerText = if (state.driveId > 0) {
+                stringResource(id = R.string.drive_name, state.driveId)
+            } else {
+                ""
+            },
+            endText = state.currentDistance,
+        ) { }
         Divider()
         LazyColumn(
             modifier = Modifier
@@ -99,39 +73,13 @@ fun MainScreenContent(
         ) {
             items(state.drives.size) { index ->
                 val drive = state.drives[index]
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .clickable { eventHandler(MainScreenEvent.DriveSelected(drive.driveId)) },
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(
-                        MaterialTheme.dimensions.rowSpaceSmall
-                    ),
+                MainScreenRow(
+                    startText = drive.driveName,
+                    centerText = drive.driveDateTime,
+                    endText =drive.driveDistance,
+                    maxLines = 2,
                 ) {
-                    EllipsizedText(
-                        modifier = Modifier
-                            .padding(vertical = MaterialTheme.dimensions.spaceMedium)
-                            .weight(1f),
-                        text = drive.driveName,
-                        style = MaterialTheme.customTypography.startBody,
-                        maxLines = 2,
-                    )
-                    EllipsizedText(
-                        modifier = Modifier
-                            .padding(vertical = MaterialTheme.dimensions.spaceMedium)
-                            .weight(1f),
-                        text = drive.driveDateTime,
-                        style = MaterialTheme.customTypography.centerBody,
-                        maxLines = 2,
-                    )
-                    EllipsizedText(
-                        modifier = Modifier
-                            .padding(vertical = MaterialTheme.dimensions.spaceMedium)
-                            .weight(1f),
-                        text = drive.driveDistance,
-                        style = MaterialTheme.customTypography.endBody,
-                        maxLines = 2,
-                    )
+                    eventHandler(MainScreenEvent.DriveSelected(drive.driveId))
                 }
                 Divider()
             }
